818c36456a8f6a31f65663daab732a05f36bdad5
Jack Ivison <jivison2001@gmail.com>
2025-01-03 14:45:03 +0000

    Added reset button feature

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -72,27 +72,31 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)])
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    //TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
-    let descirption;
+    let description;
     if (move > 0) {
-      descirption = 'Go to move #' + move;
+      description = 'Go to move #' + move;
     } else {
-      descirption = 'Go to game start';
+      description = 'Go to game start';
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{descirption}</button>
+        <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     )
   })
@@ -104,6 +108,9 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+      </div>
+      <div>
+        <button onClick={() => jumpTo(0)}>Reset</button>
       </div>
     </div>
   );
